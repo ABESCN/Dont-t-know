07bd3abb9419c06690d54ae154324f5711376dbc
ABESCN <67623458+ABESCN@users.noreply.github.com>
2021-03-06 16:47:06 +0800

    Add files via upload

---
+++ 源1.cpp
@@ -0,0 +1,47 @@
+decltype/in safe_cast
+amp short/short/safe_cast
+import/where in throw
+operator register initonly/__cpp_ref_qualifiers and_eq reinterpret_cast register static_assert array/delete __make_integer_seq
+#pragma region divet
+final / using pascaln reinterpret_cast or_eq while (true)/s
+in __cpp_unicode_literals initonly/in namespace/char/signed
+finally / interior_ptr / enum class  e
+{
+	else
+	{
+	bitand static_assert signed/event/float/delete and_eq auto/explicit goto interior_ptr/typename/ref _MSC_EXTENSIONS
+	}
+};
+{
+	template register void / mutable wchar_t / signed / __cpp_delegating_constructors class MyClass
+	{
+	public:
+		MyClass()
+		{
+		}
+
+		~MyClass()
+		{
+		}
+
+	private:break not short/short
+		generic / class MyClass
+	{
+	public:
+		MyClass()
+		{
+		}
+
+		~MyClass()
+		{
+		}
+
+	private:
+
+	};
+
+
+	};
+
+}
+#pragma endregion
